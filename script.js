f42ddede2f778c33472429794cbbd8034b5860ec
InnaSofia <inna.artemieva.mironova@mail.ru>
2023-08-01 16:03:18 +0300

    js

---
+++ script.js
@@ -0,0 +1,19 @@
+let isOpen = false
+function openMenu(){
+    const  conteinerMenu = document.getElementById("myConteinerMenu")
+    let changeImage = document.getElementById("image")
+
+
+
+    if(isOpen){
+        conteinerMenu.style.display = "none"
+        isOpen = false
+        changeImage.src = 'menu.png'
+    }else{
+        conteinerMenu.style.display = "flex"
+        isOpen = true
+        changeImage.src = 'close.png'
+    }
+
+    
+}
